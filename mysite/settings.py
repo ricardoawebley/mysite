f97fd0544ebbcdf8e5b0277d84f90a2934b9b945
RICARDOWEBLEY <success1876@outlook.com>
2021-06-23 15:35:56 -0500

    Updated url

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -99,6 +99,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+"""
 
 
 
